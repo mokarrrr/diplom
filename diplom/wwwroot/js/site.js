659d2717c3f7ed1de8d399de1aee8b616ed5283c
alexs <Alex.senin.95@mail.ru>
2023-11-07 23:28:32 +0400

    папапа

--- diplom/wwwroot/js/site.js
+++ diplom/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function (maam) {
+    $('.product-card').hover(function () {
+        $(this).find('.description').animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, 300);
+    });
+});
