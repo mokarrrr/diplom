f874379b10a2bfc127fd178afddc67ca18e05ca7
alexs <Alex.senin.95@mail.ru>
2023-11-15 16:46:08 +0400

    vvjhj

--- diplom/wwwroot/js/site.js
+++ diplom/wwwroot/js/site.js
@@ -15,3 +15,77 @@ document.getElementById('switchToRegister').addEventListener('click', function (
     document.getElementById('loginContent').style.display = 'none'; // Скрыть контент страницы входа
     document.getElementById('registerContent').style.display = 'block'; // Показать контент страницы регистрации
 });
+
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
+                var mainBodyOffset = $('.main_body').offset().top;
+
+                if (scroll >= mainBodyOffset) {
+                    $('header').addClass('header-scrolled');
+                } else {
+                    $('header').removeClass('header-scrolled');
+                }
+            });
+        });
+
+
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) {
+                    $('#backToTop').fadeIn();
+                } else {
+                    $('#backToTop').fadeOut();
+                }
+            });
+
+            $('#backToTop').click(function () {
+                $('body, html').animate({
+                    scrollTop: 0
+                }, 400);
+                return false;
+            });
+        });
+
+    
+
+        var modal = document.getElementById("modal");
+        var loginContent = document.getElementById("loginContent");
+        var registerContent = document.getElementById("registerContent");
+        var switchToRegister = document.getElementById("switchToRegister");
+        var closeButtons = document.getElementsByClassName("close");
+
+        function showModal() {
+            if (!localStorage.getItem('hasVisited')) {
+                modal.style.display = "block";
+                localStorage.setItem('hasVisited', 'true');
+            }
+        }
+
+        switchToRegister.onclick = function () {
+            loginContent.style.display = "none";
+            registerContent.style.display = "block";
+            modalContent.style.transform = "translateY(-100px)"; 
+        }
+        for (var i = 0; i < closeButtons.length; i++) {
+            closeButtons[i].onclick = function () {
+                modal.style.display = "none";
+            }
+        }
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        window.onload = function () {
+            showModal();
+        }
+
+
+
+        document.getElementById('phoneLogin', 'phoneRegister').addEventListener('input', function (e) {
+            this.value = this.value.replace(/[^\d]/g, '');
+        });
+        document.getElementById('phoneRegister').addEventListener('input', function (e) {
+            this.value = this.value.replace(/[^\d]/g, '');
+        });
